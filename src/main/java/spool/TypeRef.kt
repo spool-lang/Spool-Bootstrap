a1c0f478bdf0a1d91accd7df724ec8a9b59eac6f
RedstoneParadox <spinjitzu00@gmail.com>
2020-12-09 09:28:17 -0800

    Created ConstrainedTypeRef

--- src/main/java/spool/TypeRef.kt
+++ src/main/java/spool/TypeRef.kt
@@ -1,8 +1,22 @@
 package spool
 
-data class TypeRef(val name: String, var node: AstNode.TypeNode? = null) {
-    fun resolveType(node: AstNode.TypeNode) {
+open class TypeRef(val name: String, var node: AstNode.TypeNode? = null) {
+    open fun resolveType(node: AstNode.TypeNode) {
         if (this.node == null) this.node = node
         else throw Exception("Attempted to resolve node type has already been resolved!")
+    }
+}
+
+class ConstrainedTypeRef(private val constraints: List<TypeRef>, name: String, node: AstNode.TypeNode?): TypeRef(name, node) {
+    fun fitsConstraints(): Boolean {
+        if (constraints.size > 1) {
+            TODO("interfaces are not yet implemented.")
+        }
+
+        if (constraints.isEmpty()) {
+            return true
+        }
+
+        return constraints[0] === node?.superType
     }
 }
