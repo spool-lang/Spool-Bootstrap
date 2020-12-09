a20b0c71aba1b2b4867057e1404da16e55be820b
RedstoneParadox <spinjitzu00@gmail.com>
2020-12-09 09:37:23 -0800

    Folded constraints into the regular type ref class
    This is so we can continue to use reference equality.

--- src/main/java/spool/TypeRef.kt
+++ src/main/java/spool/TypeRef.kt
@@ -1,13 +1,11 @@
 package spool
 
-open class TypeRef(val name: String, var node: AstNode.TypeNode? = null) {
+open class TypeRef(val name: String, var node: AstNode.TypeNode? = null, private val constraints: List<TypeRef> = listOf()) {
     open fun resolveType(node: AstNode.TypeNode) {
         if (this.node == null) this.node = node
         else throw Exception("Attempted to resolve node type has already been resolved!")
     }
-}
 
-class ConstrainedTypeRef(private val constraints: List<TypeRef>, name: String, node: AstNode.TypeNode?): TypeRef(name, node) {
     fun fitsConstraints(): Boolean {
         if (constraints.size > 1) {
             TODO("interfaces are not yet implemented.")
