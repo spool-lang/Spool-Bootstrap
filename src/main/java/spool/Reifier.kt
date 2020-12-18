ee09d27762493a931ca0f1d59f8b8edc37ca6c65
RedstoneParadox <spinjitzu00@gmail.com>
2020-12-18 15:22:00 -0800

    Made progress on reification

--- src/main/java/spool/Reifier.kt
+++ src/main/java/spool/Reifier.kt
@@ -3,7 +3,7 @@ package spool
 import java.util.*
 
 class Reifier: AstVisitor<AstNode> {
-    private var currentScope = Scope(null)
+    private var currentScope = Scope()
     private val scopeStack: Stack<Scope> = Stack()
 
     override fun visitFile(file: AstNode.FileNode): AstNode {
@@ -16,7 +16,14 @@ class Reifier: AstVisitor<AstNode> {
     }
 
     override fun visitVariable(variable: AstNode.VariableNode): AstNode {
-        TODO("Not yet implemented")
+        val initializer = variable.initializer?.visit(this)
+        val typeName = variable.type.name
+
+        if (currentScope.contains(typeName)) {
+            return AstNode.VariableNode(variable.name, currentScope[typeName], variable.const, initializer)
+        }
+
+        return variable
     }
 
     override fun visitFunction(function: AstNode.FunctionNode): AstNode {
@@ -48,19 +55,39 @@ class Reifier: AstVisitor<AstNode> {
     }
 
     override fun visitBlock(block: AstNode.BlockNode): AstNode {
-        TODO("Not yet implemented")
+        if (currentScope.isEmpty()) {
+            block.statements.forEach { it.visit(this) }
+            return block
+        }
+
+        return AstNode.BlockNode(block.statements.map { it.visit(this) })
     }
 
     override fun visitIfStatement(ifStatement: AstNode.IfNode): AstNode {
-        TODO("Not yet implemented")
+        if (currentScope.isEmpty()) {
+            ifStatement.condition.visit(this)
+            ifStatement.statements.forEach { it.visit(this) }
+            ifStatement.then?.visit(this)
+            return ifStatement
+        }
+
+        return AstNode.IfNode(ifStatement.condition.visit(this), ifStatement.statements.map { it.visit(this) }, ifStatement.then?.visit(this))
     }
 
     override fun visitLoop(loop: AstNode.LoopNode): AstNode {
-        TODO("Not yet implemented")
+        if (currentScope.isEmpty()) {
+            loop.condition?.visit(this)
+            loop.incremented?.visit(this)
+            loop.incrementer?.visit(this)
+            loop.body.forEach { it.visit(this) }
+            return loop
+        }
+
+        return AstNode.LoopNode(loop.condition?.visit(this), loop.incremented?.visit(this), loop.incrementer?.visit(this), loop.body.map { it.visit(this) })
     }
 
     override fun visitJump(jump: AstNode.JumpNode): AstNode {
-        TODO("Not yet implemented")
+        return jump
     }
 
     override fun visitConstructorCall(constructorCall: AstNode.ConstructorCallNode): AstNode {
@@ -68,7 +95,16 @@ class Reifier: AstVisitor<AstNode> {
     }
 
     override fun visitFunctionCall(functionCall: AstNode.FunctionCallNode): AstNode {
-        TODO("Not yet implemented")
+        val newScope = Scope()
+        scopeStack.push(currentScope)
+        currentScope = newScope
+
+        functionCall.arguments.forEach { it.visit(this) }
+        functionCall.source.visit(this)
+
+        currentScope = scopeStack.pop()
+
+        return functionCall
     }
 
     override fun visitGenericFunctionCall(genericFunctionCall: AstNode.GenericFunctionCallNode): AstNode {
@@ -78,8 +114,9 @@ class Reifier: AstVisitor<AstNode> {
             val typeParams = source.typeParams
             val genericArguments = genericFunctionCall.genericArguments
 
+            // Technically a semantics check, but not checking it here may cause issues.
             if (typeParams.size == genericArguments.size) {
-                val newScope = Scope(currentScope)
+                val newScope = Scope()
                 scopeStack.push(currentScope)
                 currentScope = newScope
 
@@ -97,56 +134,73 @@ class Reifier: AstVisitor<AstNode> {
     }
 
     override fun visitID(id: AstNode.IdNode): AstNode {
-        TODO("Not yet implemented")
+        return id
     }
 
     override fun visitAssignment(assignment: AstNode.AssignmentNode): AstNode {
-        TODO("Not yet implemented")
+        if (currentScope.isEmpty()) {
+            assignment.value.visit(this)
+            return assignment
+        }
+
+        return AstNode.AssignmentNode(assignment.variable, assignment.value.visit(this))
     }
 
     override fun visitGet(get: AstNode.GetNode): AstNode {
-        TODO("Not yet implemented")
+        if (currentScope.isEmpty()) {
+            get.source.visit(this)
+            return get
+        }
+
+        return AstNode.AssignmentNode(get.name, get.source.visit(this))
     }
 
     override fun visitSet(set: AstNode.SetNode): AstNode {
-        TODO("Not yet implemented")
+        if (currentScope.isEmpty()) {
+            set.source.visit(this)
+            set.value.visit(this)
+            return set
+        }
+
+        return AstNode.SetNode(set.name, set.source.visit(this), set.value.visit(this))
     }
 
     override fun visitBinary(binary: AstNode.BinaryNode): AstNode {
-        binary.left.visit(this)
-        binary.right.visit(this)
+        if (currentScope.isEmpty()) {
+            binary.left.visit(this)
+            binary.right.visit(this)
+            return binary
+        }
 
-        TODO("Not yet implemented")
+        return AstNode.BinaryNode(binary.left.visit(this), binary.operator, binary.right.visit(this))
     }
 
     override fun visitUnary(unary: AstNode.UnaryNode): AstNode {
-        unary.source.visit(this)
+        if (currentScope.isEmpty()) {
+            unary.source.visit(this)
+            return unary
+        }
 
-        TODO("Not yet implemented")
+        return AstNode.UnaryNode(unary.source.visit(this), unary.operator)
     }
 
     override fun visitLiteral(literal: AstNode.LiteralNode): AstNode {
         return literal
     }
 
-    class Scope(private val parent: Scope?) {
+    class Scope {
         private val reifiedTypes: MutableMap<String, TypeRef> = mutableMapOf()
 
+        fun isEmpty(): Boolean {
+            return reifiedTypes.isEmpty()
+        }
+
         fun contains(key: String): Boolean {
-            var type = reifiedTypes[key]
-
-            if (type == null && parent != null) type = parent[key]
-
-            return type != null
+            return reifiedTypes[key] != null
         }
 
         operator fun get(key: String): TypeRef {
-            var type = reifiedTypes[key]
-
-            if (type == null && parent != null) type = parent[key]
-            if (type == null) throw Exception()
-
-            return type
+            return reifiedTypes[key] ?: throw Exception()
         }
 
         operator fun set(key: String, typeRef: TypeRef) {
