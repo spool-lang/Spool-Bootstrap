c6749c383d71e3031e0ba0f86e09299e96ecf40c
RedstoneParadox <spinjitzu00@gmail.com>
2021-02-21 11:01:16 -0800

    remove constraints for TypeRef

--- src/main/java/spool/TypeRef.kt
+++ src/main/java/spool/TypeRef.kt
@@ -1,20 +1,8 @@
 package spool
 
-open class TypeRef(val name: String, var node: AstNode.TypeNode? = null, private val constraints: List<TypeRef> = listOf()) {
+open class TypeRef(val name: String, var node: AstNode.TypeNode? = null) {
     open fun resolveType(node: AstNode.TypeNode) {
         if (this.node == null) this.node = node
         else throw Exception("Attempted to resolve node type has already been resolved!")
-    }
-
-    fun fitsConstraints(): Boolean {
-        if (constraints.size > 1) {
-            TODO("interfaces are not yet implemented.")
-        }
-
-        if (constraints.isEmpty()) {
-            return true
-        }
-
-        return constraints[0] === node?.superType
     }
 }
