8b8af9a3e3ef205dd1f7dcf2fe6723cfe5ffb6db
RedstoneParadox <spinjitzu00@gmail.com>
2021-05-24 17:33:09 -0700

    TypeResolver only throws an exception if you resolve with a different node
    Allows compilation in situations where it's resolved multiple times with the same node

--- src/main/java/spool/TypeRef.kt
+++ src/main/java/spool/TypeRef.kt
@@ -3,6 +3,6 @@ package spool
 open class TypeRef(val name: String, var node: AstNode.TypeNode? = null) {
     open fun resolveType(node: AstNode.TypeNode) {
         if (this.node == null) this.node = node
-        else throw Exception("Attempted to resolve node type has already been resolved!")
+        else if (this.node != node) throw Exception("Attempted to resolve node type has already been resolved!")
     }
 }
